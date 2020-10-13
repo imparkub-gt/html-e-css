0b95db06fa09a3e9b4f519f8942ee086fabb99ed
Thomas Raincourt <71552954+imparkub-gt@users.noreply.github.com>
2020-10-13 19:40:59 +0100

    13/!0 PM
    js e css oh e html de mask

---
+++ Sites/Scripts/js001.js
@@ -0,0 +1,2 @@
+var p1 = window.document.getElementsByTagName('p')[0]
+p1.innerHTML = 'Segundo a OMS, saúde é um estado de completo bem estar físico, psíquico, e social, <strong> e não só a ausência de doenças.'
